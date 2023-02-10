fb122cd587035fe9fcafa2582a0e9f51a6ff57c6
Mint Missy <dawidkostka480@gmail.com>
2023-02-10 11:28:43 +0100

    ci: implement authentication service

--- src/app/users/service/authentication/authentication-service.interface.ts
+++ src/app/users/service/authentication/authentication-service.interface.ts
@@ -5,7 +5,6 @@ export type NonceResponse = {
 	nonce: string;
 }
 export type LoginResponse = {};
-export type RegisterResponse = {};
 
 export abstract class AuthenticationService {
 	abstract getNonce(credentials: LoginCredentials): Observable<NonceResponse>;
--- src/app/users/service/authentication/authentication.service.ts
+++ src/app/users/service/authentication/authentication.service.ts
@@ -1,10 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AuthenticationService } from './authentication-service.interface';
+import { Observable } from 'rxjs';
+import { LoginCredentials } from '../../model/user.model';
+import { EncryptService } from '../encrypt/encrypt-service.interface';
+import { AuthenticationService, LoginResponse, NonceResponse } from './authentication-service.interface';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
-export class AuthenticationServiceImpl implements AuthenticationService  {
-	constructor(private httpClient: HttpClient) {}
+export class AuthenticationServiceImpl implements AuthenticationService {
+	readonly API_URL = 'https://api-carrental.nightly.conpeek.com/user';
+
+	constructor(private httpClient: HttpClient, private encryptService: EncryptService) {}
+
+	getNonce(credentials: LoginCredentials): Observable<NonceResponse> {
+		return this.httpClient.post<NonceResponse>(`${this.API_URL}/login`, { login: credentials.email });
+	}
+
+	login(credentials: LoginCredentials, nonce: string): Observable<LoginResponse> {
+		let encryptedPassword = this.encryptService.encrypt(credentials.password);
+		encryptedPassword = this.encryptService.encrypt(encryptedPassword + nonce);
+
+		const payload = { login: credentials.email, password: encryptedPassword };
+
+		return this.httpClient.post<LoginResponse>(`${this.API_URL}/login`, payload);
+	}
 }
